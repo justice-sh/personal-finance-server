6775d3551adef2096c4be540d44dc33290cf6a92
Justice <sherllyj191@gmail.com>
2025-07-21 09:52:12 +0100

    Enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,16 @@
-import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import { NestFactory } from "@nestjs/core";
 import * as cookieParser from "cookie-parser";
 import { VersioningType } from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ["http://localhost:3000"],
+    credentials: true,
+  });
+
   app.use(cookieParser());
 
   app.enableVersioning({
