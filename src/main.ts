d4e07462159bb756d690d115896f301822724687
Justice <sherllyj191@gmail.com>
2025-07-28 06:44:44 +0100

    Updated

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000"], // TODO: use environment variable for production
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    allowedHeaders: "Content-Type, Accept, Authorization",
     credentials: true,
   });
 
@@ -15,7 +17,7 @@ async function bootstrap() {
 
   app.enableVersioning({
     type: VersioningType.URI,
-    defaultVersion: "1",
+    defaultVersion: "1", // TODO: use environment variable for production
     prefix: "api/v",
   });
 
